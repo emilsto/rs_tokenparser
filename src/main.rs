e3648376c265fef739f3258a05ffc9c800bcd26b
emil <emil.kantaneva@gmail.com>
2023-05-13 17:38:16 +0300

    Refactor read_token function to return a Result type

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,17 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let file_path: &String = &args[1];
 
-    let token : String= read_token(file_path);
-
-    println!("{token}");
-
-    parse_token(&token);
+    let contents = read_token(file_path).unwrap();
+    println!("{}", contents);
 
 
 
 }
 
-fn read_token(file_path : &String) -> String {
+fn read_token(file_path : &String) -> Result<String, std::io::Error> {
 
-    let contents: String = fs::read_to_string(file_path)
-    .expect("Should have been able to read the file");
-
-
-    return contents;
+    let contents = fs::read_to_string(file_path)?;
+    Ok(contents)
 
 }
 
